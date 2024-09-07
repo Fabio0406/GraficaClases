b62d8c8dd9483d00308728a60d03621bddbb33e0
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-09-06 22:39:09 -0400

    first commit

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -8,6 +8,7 @@ using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 using ConsoleApp1.Clases;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -18,160 +19,37 @@ namespace ConsoleApp1
 
         public Game() : base(800, 600) // Constructor que define el tamaño de la ventana
         {
-            Escenario1=new Escenario(0.0f,0.0f,0.0f);
-            Objeto t=new Objeto(0.0f, 0.0f, 0.0f);
-            
-            //Partes de la T
-            Parte Tsuperior = new Parte();
-            Parte Tinferior = new Parte(); 
-            //Caras
-            Cara TSCFrente = new Cara();
-            Cara TSCSuperior = new Cara();
-            Cara TSCInferior = new Cara();
-            Cara TSCPosterior = new Cara();
-            Cara TSCDerecha = new Cara();
-            Cara TSCIzquierda = new Cara();
+            // Leer el JSON desde el archivo
+            string json = File.ReadAllText(@"C:\Users\fabio\Documents\UAGRM\Semestre 2-2024\Grafica\ProgramGrafica-Tarea3\ConsoleApp1\escenario.txt");
 
-            Cara TICFrente = new Cara();
-            Cara TICSuperior = new Cara();
-            Cara TICInferior = new Cara();
-            Cara TICPosterior = new Cara();
-            Cara TICDerecha = new Cara();
-            Cara TICIzquierda = new Cara();
+            // Deserializar el JSON en el objeto Escenario1
+            Escenario1 = JsonConvert.DeserializeObject<Escenario>(json);
 
-            //Armar el escenario
-            Escenario1.AgregarObjeto("T", t);
-
-
-            //Ensamblar T
-            t.AgregarParte("Superior", Tsuperior);
-            t.AgregarParte("Inferior", Tinferior);
-            
-
-           
-
-            //Ensamblar partes
-            Tsuperior.AgregarCara("frente", TSCFrente);
-            Tsuperior.AgregarCara("superior",TSCSuperior);
-            Tsuperior.AgregarCara("inferior", TSCInferior);
-            Tsuperior.AgregarCara("posterior", TSCPosterior);
-            Tsuperior.AgregarCara("derecha", TSCDerecha);
-            Tsuperior.AgregarCara("izquierda", TSCIzquierda);
-
-            Tinferior.AgregarCara("frente", TICFrente);
-            Tinferior.AgregarCara("superior", TICSuperior);
-            Tinferior.AgregarCara("inferior", TICInferior);
-            Tinferior.AgregarCara("posterior", TICPosterior);
-            Tinferior.AgregarCara("derecha", TICDerecha);
-            Tinferior.AgregarCara("izquierda", TICIzquierda);
-
-
-            //Vertices
-            Vertice[] v1 = {
-                new Vertice(-4, 3.5, 2),
-                new Vertice(4, 3.5, 2), 
-                new Vertice(4, -1, 2),
-                new Vertice(- 4, -1, 2)
-            };
-            TSCFrente.LoadVertices(v1);
-
-            Vertice[] v2 = {
-                new Vertice(4, 3.5, 2),
-                new Vertice(4, 3.5, -2),
-                new Vertice(4, -1, -2),
-                new Vertice(4, -1, 2)
-            };
-            TSCDerecha.LoadVertices(v2);
-
-            Vertice[] v3 = {
-                new Vertice(-4, 3.5, -2),
-                new Vertice(4, 3.5, -2),
-                new Vertice(4, -1, -2),
-                new Vertice(-4, -1, -2)
-            };
-            TSCPosterior.LoadVertices(v3);
-
-            Vertice[] v4 = {
-                new Vertice(-4, 3.5, 2),
-                new Vertice(-4, 3.5, -2),
-                new Vertice(-4, -1, -2),
-                new Vertice(-4, -1, 2)
-            };
-            TSCIzquierda.LoadVertices(v4);
-
-            Vertice[] v5 = {
-                new Vertice(-4, 3.5, 2),
-                new Vertice(-4, 3.5, -2),
-                new Vertice(4, 3.5, -2),
-                new Vertice(4, 3.5, 2)
-            };
-            TSCSuperior.LoadVertices(v5);
-
-            Vertice[] v6 = {
-                new Vertice(-4, -1, 2),
-                new Vertice(-4, -1, -2),
-                new Vertice(4, -1, -2),
-                new Vertice(4, -1, 2)
-            };
-            TSCInferior.LoadVertices(v6);
-
-
-
-            Vertice[] v7 = {
-                new Vertice(-1.5, 3.5, 2),
-                new Vertice(1.5, 3.5, 2),
-                new Vertice(1.5, -10, 2),
-                new Vertice(-1.5, -10, 2)
-            };
-            TICFrente.LoadVertices(v7);
-
-            Vertice[] v8 = {
-                new Vertice(1.5, 3.5, 2),
-                new Vertice(1.5, 3.5, -2),
-                new Vertice(1.5, -10, -2),
-                new Vertice(1.5, -10, 2)
-            };
-            TICDerecha.LoadVertices(v8);
-
-            Vertice[] v9 = {
-                new Vertice(-1.5, 3.5, -2),
-                new Vertice(1.5, 3.5, -2),
-                new Vertice(1.5, -10, -2),
-                new Vertice(-1.5, -10, -2)
-            };
-            TICPosterior.LoadVertices(v9);
-
-            Vertice[] v10 = {
-                new Vertice(-1.5, 3.5, 2),
-                new Vertice(-1.5, 3.5, -2),
-                new Vertice(-1.5, -10, -2),
-                new Vertice(-1.5, -10, 2)
-            };
-            TICIzquierda.LoadVertices(v10);
-
-            Vertice[] v11 = {
-                new Vertice(-1.5, 3.5, 2),
-                new Vertice(-1.5, 3.5, -2),
-                new Vertice(1.5, 3.5, -2),
-                new Vertice(1.5, 3.5, 2)
-            };
-            TICSuperior.LoadVertices(v11);
-
-            Vertice[] v12 = {
-                new Vertice(-1.5, -10, 2),
-                new Vertice(-1.5, -10, -2),
-                new Vertice(1.5, -10, -2),
-                new Vertice(1.5, -10, 2)
-            };
-            TICInferior.LoadVertices(v12);
-
+            // Serializa el escenario
+            //SerializarEscenario(@"C:\Users\fabio\Documents\UAGRM\Semestre 2-2024\Grafica\ProgramGrafica-Tarea3\ConsoleApp1\escenario.txt");
         }
 
-        
+        private void SerializarEscenario(string filePath)
+        {
+            try
+            {
+                // Serializar el escenario a formato JSON
+                string json = JsonConvert.SerializeObject(Escenario1, Formatting.Indented);
+
+                // Guardar el JSON en un archivo .txt
+                File.WriteAllText(filePath, json);
+
+                Console.WriteLine("Escenario serializado exitosamente en: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error serializando el escenario: " + ex.Message);
+            }
+        }
+
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-
             GL.ClearColor(Color.FromArgb(0, 100, 0));
             GL.Enable(EnableCap.DepthTest); // Profundidad
         }
@@ -179,14 +57,12 @@ namespace ConsoleApp1
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
-
             rotationAngle += 6.5f * (float)e.Time; // Rotación
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
-
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
@@ -207,8 +83,6 @@ namespace ConsoleApp1
         protected override void OnResize(EventArgs e)
         {
             base.OnResize(e);
-
-            // Ajustar la vista en caso de que se redimensione la ventana
             GL.Viewport(0, 0, Width, Height);
         }
     }
